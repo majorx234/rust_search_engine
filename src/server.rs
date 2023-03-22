39879c754dbe5b3bd8db3f256e07865cec387167
Majorx234 <majorx234@googlemail.com>
2023-03-23 00:36:33 +0100

    Parse json search request and search in index and reply results

--- src/server.rs
+++ src/server.rs
@@ -50,9 +50,28 @@ pub fn serve_search(index: &TermIndex, mut request: Request) -> io::Result<()> {
     let mut buf = String::new();
     if let Err(err) = request.as_reader().read_to_string(&mut buf) {
         eprintln!("ERROR: could not read the body of the request: {err}");
-        // return serve_500(request);
+        return serve_500(request);
     }
-
     println!("body: {}", buf);
+    let body_json_data = json::parse(&buf).unwrap();
+    if let Some(search_input) = body_json_data["search_input"].as_str() {
+        let search_input_chars: Vec<char> = search_input.chars().collect();
+        let result = match index.search_query(&search_input_chars) {
+            Ok(result) => result,
+            Err(()) => return serve_500(request),
+        };
+
+        let json = match serde_json::to_string(&result.iter().take(20).collect::<Vec<_>>()) {
+            Ok(json) => json,
+            Err(err) => {
+                eprintln!("ERROR: could not convert search results to JSON: {err}");
+                return serve_500(request);
+            }
+        };
+
+        let content_type_header = Header::from_bytes("Content-Type", "application/json")
+            .expect("That we didn't put any garbage in the headers");
+        return request.respond(Response::from_string(&json).with_header(content_type_header));
+    }
     Ok(())
 }
