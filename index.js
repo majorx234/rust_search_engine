ad7734a5d9f2e8bcbaaf737b48962e81f521557f
Majorx234 <majorx234@googlemail.com>
2023-03-25 15:21:40 +0100

    Add formatted output of results in table

--- index.js
+++ index.js
@@ -6,6 +6,41 @@ function outputToConsole(text) {
     para.scrollIntoView();
 }
 
+function tableOutputToConsole(json_data, row_header) {
+    let datensaetze = json_data;
+    let tbl = document.createElement("table");
+    let tblBody = document.createElement("tbody");
+    let tblheader = document.createElement("thead");
+    let tblh_tr = document.createElement("tr");
+
+    for (let x = 0; x < row_header.length; x++) {
+        const row_header_text = document.createTextNode(row_header[x]);
+        let tblh_element = document.createElement("th");
+        tblh_element.appendChild(row_header_text);
+        tblh_tr.appendChild(tblh_element);
+    }
+
+    tblheader.appendChild(tblh_tr);
+    tbl.appendChild(tblheader);
+
+    for(let i = 0; i < datensaetze.length; i++) {
+        const row = document.createElement("tr");
+
+        for (let j = 0; j < datensaetze[i].length; j++) {
+            const cell = document.createElement("td");
+            const cellText = document.createTextNode(datensaetze[i][j] );
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+        tblBody.appendChild(row);
+    }
+    tbl.appendChild(tblBody);
+    tbl.setAttribute("border", "2");
+    tbl.setAttribute("id","output_table");
+    document.getElementById("console").prepend(tbl);
+}
+
+
 function httpGet(endpoint_name, variable_context, response_handler) {
     let xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
@@ -48,8 +83,10 @@ function httpPostSearch() {
     outputToConsole(search_input);
     let param_name = "search_input";
     let response_handler = (response_text) => {
+        let table_header = ["path", "rank"];
         let json_data = JSON.parse(response_text);
-        outputToConsole(variable_context + json_data);
+        outputToConsole(variable_context);
+        tableOutputToConsole(json_data, table_header);
     };
     httpPost(end_point_name, param_name, search_input, variable_context, response_handler);
 }
